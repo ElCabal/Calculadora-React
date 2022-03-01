bd92f89f2c058fdf2e2d0a901c9c4873cbea6cc9
ElCabal <cabalholguinlufe@gmail.com>
2022-02-28 19:00:39 -0500

    inicio de proyecto calculadora

---
+++ src/Components/Input.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../Styles/Input.css";
+
+export const Input = () => {
+  return (
+    <div className="container_input">
+      <input type="number" className="input" />
+    </div>
+  );
+};
---
+++ src/Components/Title.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Title = () => (
+  <>
+    <h1>CALCULADORA</h1>
+  </>
+);
